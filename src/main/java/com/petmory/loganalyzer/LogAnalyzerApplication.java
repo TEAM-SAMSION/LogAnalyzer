d1b06fe5f4d4ab7bb5983d721f38c12454fe5606
tlarbals824 <simkm0824@naver.com>
2023-07-26 16:30:22 +0900

    PET-35 feat : Reactive Mongo 설정 추가

--- src/main/java/com/petmory/loganalyzer/LogAnalyzerApplication.java
+++ src/main/java/com/petmory/loganalyzer/LogAnalyzerApplication.java
@@ -2,8 +2,12 @@ package com.petmory.loganalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableMongoAuditing
 public class LogAnalyzerApplication {
 
     public static void main(String[] args) {
