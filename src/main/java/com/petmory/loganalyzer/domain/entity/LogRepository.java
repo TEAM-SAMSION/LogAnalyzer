6826320e56422fcf43a03da1af48449d2a937d10
tlarbals824 <simkm0824@naver.com>
2023-07-26 16:30:02 +0900

    PET-35 feat : 로그 타입(INFO, ERROR), 로그 id(Log Id)를 이용한 검색 기능 추가

--- src/main/java/com/petmory/loganalyzer/domain/entity/LogRepository.java
+++ src/main/java/com/petmory/loganalyzer/domain/entity/LogRepository.java
@@ -1,6 +1,10 @@
 package com.petmory.loganalyzer.domain.entity;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface LogRepository extends ReactiveMongoRepository<Log, String> {
+    Flux<Log> findAllByLogType(LogType logType);
+
+    Flux<Log> findAllByLogId(String logId);
 }
--- src/main/java/com/petmory/loganalyzer/domain/service/LogService.java
+++ src/main/java/com/petmory/loganalyzer/domain/service/LogService.java
@@ -1,5 +1,6 @@
 package com.petmory.loganalyzer.domain.service;
 
+import com.petmory.loganalyzer.domain.entity.LogType;
 import com.petmory.loganalyzer.domain.service.dto.request.SaveLogRequest;
 import com.petmory.loganalyzer.domain.service.dto.response.InfoLogResponse;
 import reactor.core.publisher.Flux;
@@ -8,5 +9,7 @@ import reactor.core.publisher.Mono;
 public interface LogService {
     Mono<Void> saveLog(SaveLogRequest request);
 
-    Flux<InfoLogResponse> getLogs();
+    Flux<InfoLogResponse> getLogsByLogType(LogType logType);
+
+    Flux<InfoLogResponse> getLogById(String logId);
 }
